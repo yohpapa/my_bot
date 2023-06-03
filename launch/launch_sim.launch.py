0038f7a4343f60b596ea4d34f4161fcf4fb63bdc
yohpapa <kemumaki.kemuo@gmail.com>
2023-06-03 22:33:59 +0900

    Supported ros2_control with gazebo.

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -8,7 +8,7 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
-
+from launch.actions import TimerAction
 
 
 def generate_launch_description():
@@ -22,14 +22,21 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), 
+                launch_arguments={'use_sim_time': 'true',
+                                  'use_ros2_control': 'true'}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo_params_path = os.path.join(
+                  get_package_share_directory(package_name),'config','gazebo_params.yaml')
+                  
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+                launch_arguments={
+                    'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path }.items()
+                )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -37,11 +44,29 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
-
+    # Launch ROS2 controllers
+    controllers = TimerAction(
+        period=5.0,
+        actions=[
+            Node(package='controller_manager',
+                 executable='spawner',
+                 arguments=['diff_cont']),
+            Node(package='controller_manager',
+                 executable='spawner',
+                 arguments=['joint_broad'])
+            ]
+    )
+    # diff_drive_spawner = Node(package='controller_manager',
+    #                           executable='spawner',
+    #                           arguments=['diff_cont'])
+    # joint_broad_spawner = Node(package='controller_manager',
+    #                           executable='spawner',
+    #                           arguments=['joint_broad'])
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        controllers,
     ])
